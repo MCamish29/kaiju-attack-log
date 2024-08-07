bc16437a75ed92a71f2ea2c07bd258bf44470134
MCamish29 <mcamish29@gmail.com>
2024-08-07 18:59:38 +0100

    "Add user request loop to repeat request for valid data"

--- run.py
+++ run.py
@@ -17,17 +17,20 @@ def get_date_data():
     """
     Get date of Kaiju attack input from the user
     """
-    print("Please enter date of Kaiju attack.")
-    print("Date format should be dd/mm/yyyy.")
-    print("Example: 01/01/2024\n")
+    while True:
+        print("Please enter date of Kaiju attack.")
+        print("Date format should be dd/mm/yyyy.")
+        print("Example: 01/01/2024\n")
 
-    data_str = input("Enter date of Kaiju attack here: ")
+        data_str = input("Enter date of Kaiju attack here: ")
 
-    if validate_date(data_str):
-        print(f"Confirmed, date of attack is {data_str}")
-    else:
-        print("Invalid date format. Please try again.")
+        if validate_date(data_str):
+            print(f"Confirmed, date of attack is {data_str}")
+            break
 
+    return data_str           
+            
+    
 def validate_date(date_str):
     """
     Raises ValueError if the date input is not in required format
@@ -36,6 +39,7 @@ def validate_date(date_str):
         datetime.strptime(date_str, '%d/%m/%Y')
         return True
     except ValueError:
+        print("Invalid date format. Please try again.\n")
         return False
 
-get_date_data()
+data = get_date_data()
